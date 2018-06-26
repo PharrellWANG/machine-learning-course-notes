e0d5213c9b73d3c39df9c6660c15a5aaca58e9eb
PharrellWANG <wzxnuaa@gmail.com>
2018-06-26 11:13:46 +0800

    remote _build_html folder

--- _build_html/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({docnames:["index"],envversion:53,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{},titles:["Machine Learning Course Notes"],titleterms:{cours:0,learn:0,machin:0,note:0}})
